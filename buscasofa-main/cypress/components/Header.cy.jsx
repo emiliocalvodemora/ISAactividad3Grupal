a7e17eefe5d9edb7a06c953a91d7bb04a416e39b
Emilio Calvo de Mora Mármol <emiliocalvodemoramarmol@gmail.com>
2025-06-01 20:41:12 +0200

    Commit con proyecto base de ejempo

---
+++ buscasofa-main/cypress/components/Header.cy.jsx
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+// import React from 'react'
+import Header from '../../src/components/Header'
+
+describe('<Header />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Header />)
+  })
+})
