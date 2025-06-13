34d61c6ed52437108a402d810e1e30fcfe0914a5
didasa1974 <didisa@gmail.com>
2025-06-13 10:01:23 +0200

    mejora

--- cypress/e2e/features/register/index.js
+++ cypress/e2e/features/register/index.js
@@ -1,52 +1,54 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-const randomUser = () => ({
-  username: `testuser${Date.now()}`,
-  email: `test${Date.now()}@mail.com`,
-  password: 'Test1234!'
-});
-
-let user = randomUser();
-
 Given('el usuario navega a la página de registro', () => {
-  cy.visit('/registro').wait(1000);
+    cy.visit('/registro').wait(1000);
 });
 
-Given('el servidor está caído', () => {
-  cy.intercept('POST', 'http://localhost:4000/api/register', {
-    forceNetworkError: true,
-  }).as('registroFallido');
+Then('ve los campos {string}, {string}, {string}', () => {
+    cy.get('input[name="username"]').should('exist');
+    cy.get('input[name="email"]').should('exist');
+    cy.get('input[name="password"]').should('exist');
 });
 
-Then('ve los campos {string}, {string}, {string}', (campo1, campo2, campo3) => {
-  cy.get('input[name="username"]').should('exist');
-  cy.get('input[name="email"]').should('exist');
-  cy.get('input[name="password"]').should('exist');
-});
-
-Then('ve el botón {string}', (textoBoton) => {
-  cy.get('button').contains(textoBoton).should('exist');
+Then('ve el botón {string}', () => {
+    cy.contains('button', 'Registrarse').should('exist');
 });
 
 When('completa el formulario de registro con datos válidos', () => {
-  cy.get('input[name="username"]').clear().type(user.username);
-  cy.get('input[name="email"]').clear().type(user.email);
-  cy.get('input[name="password"]').clear().type(user.password);
+    cy.get('input[name="username"]').clear().type('usuarioTest');
+    cy.get('input[name="email"]').clear().type('test@mail.com');
+    cy.get('input[name="password"]').clear().type('Password123!');
 });
 
-When('hago clic en {string}', (textoBoton) => {
-  cy.get('button').contains(textoBoton).click();
+When('hago clic en {string}', () => {
+    cy.contains('button', 'Registrarse').click();
 });
 
 Then('ve un mensaje de confirmación de registro', () => {
-  cy.contains(/usuario registrado correctamente|registro exitoso/i, { timeout: 5000 }).should('be.visible');
+    cy.contains(/usuario registrado correctamente|registro exitoso/i, { timeout: 5000 })
+      .should('exist');
 });
 
-Then('debería ver un mensaje de error', () => {
-  cy.contains(/error|no se pudo registrar/i, { timeout: 5000 }).should('be.visible');
+Then('muestra error si el servidor está caído', () => {
+    cy.intercept('POST', 'http://localhost:4000/api/register', {
+        forceNetworkError: true,
+    }).as('registroFallido');
+
+    cy.get('input[name="username"]').type('usuarioTest');
+    cy.get('input[name="email"]').type('test@mail.com');
+    cy.get('input[name="password"]').type('Password123!');
+    cy.contains('button', 'Registrarse').click();
+
+    cy.contains(/error en el registro|network error/i, { timeout: 5000 })
+      .should('exist');
 });
 
-Then('ve un mensaje de campos obligatorios', () => {
-  cy.contains(/todos los campos son obligatorios/i).should('be.visible');
+Then('muestra error por campos vacíos', () => {
+    cy.get('input[name="username"]').clear();
+    cy.get('input[name="email"]').clear();
+    cy.get('input[name="password"]').clear();
+
+    cy.contains('button', 'Registrarse').click();
+    cy.contains('Todos los campos son obligatorios').should('exist');
 });
 
